598e2118877794302ec35420db636affe8a7132d
Nicolas-Velasquez <125744775+Nicolas-Velasquez@users.noreply.github.com>
2024-05-18 13:10:40 -0500

    Se agregaron cambios

---
+++ index.js
@@ -0,0 +1,10 @@
+function alerta() {
+    // Muestra un cuadro de confirmación al usuario
+    if (confirm("¿Estás seguro que deseas salir?")) {
+        // Si el usuario confirma, redirige a "resultado.html"
+        window.location.href = "cosas.html";
+    } else {
+        // Si el usuario cancela, recarga la página actual
+        window.location.reload();
+    }
+}
